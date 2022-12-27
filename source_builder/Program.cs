605654339bef319634ebfd77932d685a10b43296
Ara Malayan <aramalayan@gmail.com>
2022-12-27 15:45:24 -0800

    Migrate project to .NET 7.0

--- source_builder/Program.cs
+++ source_builder/Program.cs
@@ -15,6 +15,7 @@ namespace UDP_Patcher
         static void Main()
         {
             Application.EnableVisualStyles();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
